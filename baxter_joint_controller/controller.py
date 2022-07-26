b41c85d839a4fc389d8980749b945fc6f537fb2b
09ubberboy90 <2330834a@student.gla.ac.uk>
2022-07-26 19:14:23 +0100

    Add gripper controller

--- baxter_joint_controller/controller.py
+++ baxter_joint_controller/controller.py
@@ -1,8 +1,13 @@
+import time
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState
+
+from baxter_joint_controller.gripper import Gripper
+from rclpy.executors import MultiThreadedExecutor
+import threading
 
 from baxter_core_msgs.msg import (
     JointCommand,
@@ -28,21 +33,40 @@ class MinimalPublisher(Node):
         super().__init__('minimal_publisher')
         self.left_publisher = self.create_publisher(JointCommand, '/robot/limb/left/joint_command', 10)
         self.right_publisher = self.create_publisher(JointCommand, '/robot/limb/right/joint_command', 10)
+        self._gripper_left = Gripper("left", self, False)
+        self._gripper_right = Gripper("right", self, False)
+        self.left_thread = threading.Thread(target=self.init_gripper, args=(self._gripper_left,))
+        self.left_thread.start()
+        self.right_thread = threading.Thread(target=self.init_gripper, args=(self._gripper_right,))
+        self.right_thread.start()
+
         self.subscription = self.create_subscription(
             JointState,
             'joint_states_sim',
             self.listener_callback,
             10)
         self.joint_states = {}
-        timer_period = 0.05  # seconds
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
         self._joint_names = {
             'left': ['left_s0', 'left_s1', 'left_e0', 'left_e1',
                      'left_w0', 'left_w1', 'left_w2'],
             'right': ['right_s0', 'right_s1', 'right_e0', 'right_e1',
-                      'right_w0', 'right_w1', 'right_w2']
+                      'right_w0', 'right_w1', 'right_w2'],
+            # 'left_gripper' : ["l_gripper_l_finger_joint",
+            #                 "l_gripper_r_finger_joint",],
+            # 'right_gripper' : ["r_gripper_l_finger_joint",
+            #                 "r_gripper_r_finger_joint",]
             }
+
+    def init_gripper(self, gripper:Gripper):
+        while not (gripper.state_init and gripper.prop_init):
+            time.sleep(.1)
+            print("Thread Sleep")
+        if gripper.calibrate():
+            print(f"Calibrating successfull for {gripper.name}")
+        return
         
 
     def set_joint_positions(self, positions, hand="right", raw=False ):
@@ -60,7 +84,6 @@ class MinimalPublisher(Node):
         command_msg = JointCommand()
         if hand not in ["right", "left"]:
             return command_msg
-        print(f"{hand}:{positions}")
         command_msg.names = self._joint_names[hand]
         command_msg.command = positions
         if raw:
@@ -73,23 +96,33 @@ class MinimalPublisher(Node):
         for name, pose in zip(msg.name, msg.position):
             self.joint_states[name] = pose
 
+    def convert_range(self,value):
+        # print(f"Original : {value}, New : {(value * 100 / 0.020833)}")
+        return (value * 100 / 0.020833)
+
+
     def timer_callback(self):
         self.right_publisher.publish(self.set_joint_positions([y for x, y in self.joint_states.items() if x in self._joint_names["right"]], hand="right"))
         self.left_publisher.publish(self.set_joint_positions([y for x, y in self.joint_states.items() if x in self._joint_names["left"]], hand="left"))
-
+        if self._gripper_left.calibrated != True:
+            try:
+                self._gripper_left.command_position(self.convert_range(self.joint_states["l_gripper_l_finger_joint"]))
+            except KeyError:
+                pass
+        if self._gripper_right.calibrated != True:
+            try:
+                self._gripper_right.command_position(self.convert_range(self.joint_states["r_gripper_l_finger_joint"]))
+            except KeyError:
+                pass
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    node = MinimalPublisher()
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    executor.spin()
     rclpy.shutdown()
 
 
